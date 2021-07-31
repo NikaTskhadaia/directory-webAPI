78d901d1f2ec0427924d92f3147345bf49cda825
Nika <nika.tskhadaia@gmail.com>
2021-07-31 15:22:47 +0400

    init: initial commit

---
+++ Repository/Repository/IPersonRepository.cs
@@ -0,0 +1,22 @@
+﻿using Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Repository
+{
+    public interface IPersonRepository
+    {
+        void AddPerson(Person person);
+        void ChangePerson(Person person);
+        void UploadPesonImage();
+        void RemovePerson(Person person);
+        void AddRelatedPerson(Person person, Person relatedPerson);
+        void RemoveRelatedPerson(Person person, Person relatedPerson);
+        Person GetPerson(int id);
+        IEnumerable<Person> GetPeople();
+        int RelatedPersonCount(Person person, Relations relation);
+    }
+}
---
+++ Repository/Repository/PersonRepository.cs
@@ -0,0 +1,69 @@
+﻿using Repository.Data;
+using Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Repository
+{
+    class PersonRepository : IPersonRepository
+    {
+        private readonly PeopleDb _db;
+
+        public PersonRepository(PeopleDb db)
+        {
+            _db = db;
+        }
+        public void AddPerson(Person person)
+        {
+            _db.People.Add(person);
+        }
+
+        public void AddRelatedPerson(Person person, Person relatedPerson)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangePerson(Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Person> GetPeople()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Person GetPerson(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int RelatedPersonCount(Person person, Relation relation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int RelatedPersonCount(Person person, Relations relation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemovePerson(Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRelatedPerson(Person person, Person relatedPerson)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UploadPesonImage()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
